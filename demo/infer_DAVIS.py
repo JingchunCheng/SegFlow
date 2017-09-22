7fd7f55d9b2faaded94ed6c3d463ed10c7760570
Jingchun Cheng <chengjingchun14@163.com>
2017-09-22 14:29:50 -0700

    Update infer_DAVIS.py

--- demo/infer_DAVIS.py
+++ demo/infer_DAVIS.py
@@ -33,7 +33,7 @@ def load_image(im_name):
 cls_name = sys.argv[1]
 
 davis_dir = '../data/DAVIS/'
-split_f  = '{}/ImageSets/480p/train.txt'.format(davis_dir)
+
 
 caffe_model  = '../model/SegFlow.caffemodel'
 deploy_proto = '../model/deploy.prototxt'
